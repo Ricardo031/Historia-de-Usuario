c03a0e2b0cb34c543e15470eca2ada7a1b2794c3
Ricardo031 <rjtb31@gmail.com>
2026-03-17 17:00:02 -0500

    Corecciones

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-#Genaracion de ID 
+# ==============================
+#* Genaracion de ID 
+# ==============================
 def id_generate(dictionary):
     if dictionary:
         return max(dictionary.keys()) + 1
@@ -6,7 +8,7 @@ def id_generate(dictionary):
         return 1
     
 # ==============================
-# Registrar Clientes
+#* Registrar Clientes
 # ==============================
 def register_clients(clients,name,email):
     client_id = id_generate(clients)
@@ -18,8 +20,9 @@ def register_clients(clients,name,email):
 
     return client_id, clients
 
-#Todo: agregar lo demas 
-
+# ==============================
+#* Registrar Productos
+# ==============================
 def register_products(products, name, price):
     product_id = id_generate(products)
 
@@ -27,6 +30,7 @@ def register_products(products, name, price):
     products[product_id] = product
     return product_id, products
 
+#%Todo: agregar lo demas 
 
 
 
@@ -38,10 +42,9 @@ ordes = {}
 
 op = 1
 while op != 0:
-    print("Customer order Management System ")
+    print("\033[1;32mCustomer order Management System \033[0m")
     print("======== MENU =========")
-    print("1.Register clients\n 2.Register Products\n 3.Create Order\n 4.Check Order\n 5.Calculate income\n 6.final report\n 0.exit" )
-
+    print(" 1.Register clients\n 2.Register Products\n 3.Create Order\n 4.Check Order\n 5.Calculate income\n 6.final report\n 0.exit" )
 
     option = int(input("choose a option: "))
 
@@ -52,6 +55,7 @@ while op != 0:
 
         print(f"registered client {id_clients}" )
         print("------------------------------------------------")
+
     elif option == 2:
         name_product = input("into name: ")
         price_product = float(input("Into price: "))
